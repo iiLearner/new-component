7061f25eb6dcc6ee305375afcd3314c0537694ad
iLearner <me@i-learner.it>
2022-04-07 12:01:53 +0200

    feat: comp test id

--- src/templates/functional.js
+++ src/templates/functional.js
@@ -7,7 +7,7 @@ interface COMPONENT_NAMEProps {
 
 const COMPONENT_NAME = (props: COMPONENT_NAMEProps) => {
   return (
-    <div data-testid={COMPONENT_NAME}>
+    <div data-testid="COMPONENT_NAME">
       <span>COMPONENT_NAME</span>
     </div>
   )
